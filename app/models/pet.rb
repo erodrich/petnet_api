6ebe38840119940016b2650f0bd32b338a4c6cba
Eric Rodrich <acron.bn@gmail.com>
2017-05-02 18:32:21 -0500

    User send

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
-    has_one :post, dependent: :destroy
+    has_one :post, dependent: :delete
     
     validates_presence_of :name, :animal, :breed, :created_by
 end
